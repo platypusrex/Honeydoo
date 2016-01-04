cfdb1e09f9cd72f03be54238fb23a05477bbb466
platypusrex <frankcooke79@gmail.com>
2016-01-03 21:47:59 -0500

    created functionality for storing user data on user registration

--- app/components/auth/authController.js
+++ app/components/auth/authController.js
@@ -2,16 +2,17 @@
     'use strict';
 
     authModule.controller('authCtrl', ['$scope', '$location', 'authService', 'firebaseDataService', function($scope, $location, authService, firebaseDataService){
-
         $scope.register = function(user){
             authService.register(user)
                 .then(function(userData){
+                    $location.path('/form');
                     var ref = firebaseDataService.users;
                     var uid = userData.uid;
 
                     ref.child(uid).set({
                         email: user.email,
-                        username: user.username
+                        username: user.username,
+                        gender: user.gender
                     });
 
                     return $scope.user;
