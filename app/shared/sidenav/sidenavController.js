42dbe7776934acbda3f60e11645768fa43c44355
platypusrex <frankcooke79@gmail.com>
2016-01-13 23:46:38 -0500

    making some style changes to the sidenav

--- app/shared/sidenav/sidenavController.js
+++ app/shared/sidenav/sidenavController.js
@@ -3,12 +3,26 @@
 
     sidenavModule.controller('sidenavCtrl', ['$scope', 'sidenavService', function($scope, sidenavService){
         $scope.user = sidenavService.getUserAuth();
+        $scope.iconActive1 = false;
+        $scope.iconActive2 = false;
+        $scope.icon1 = 1;
+        $scope.icon2 = 2;
+
+        $scope.iconToggle = function(val){
+            if(val === 1){
+                $scope.iconActive1 = !$scope.iconActive1;
+            }
+            if(val === 2){
+                $scope.iconActive2 = !$scope.iconActive2;
+            }
+        };
 
         if($scope.user){
             var userData = sidenavService.getUserData($scope.user.uid);
             userData.$loaded(
                 function(data){
                     $scope.username = data.username;
+                    $scope.userIcon = (data.gender === 'his');
                 },
                 function(error){
                     console.log(error);
