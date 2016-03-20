1a5d4e50e7197b6209e74e148e7f2f0b350dd442
platypusrex <frankcooke79@gmail.com>
2016-03-20 15:00:19 -0400

    adds scroll bottom directive to the honeychat modal, but not working when you first open modal

---
+++ app/components/honeyChat/scrollBottomDirective.js
@@ -0,0 +1,18 @@
+(function(honeyChatModule){
+    'use strict';
+
+    honeyChatModule.directive('scrollBottom', function(){
+        return {
+            scope: {
+                scrollBottom: '='
+            },
+            link: function(scope, element){
+                scope.$watchCollection('scrollBottom', function(newValue){
+                    if(newValue){
+                        $(element).scrollTop($(element)[0].scrollHeight);
+                    }
+                });
+            }
+        }
+    });
+}(angular.module('HoneyChatModule')));
