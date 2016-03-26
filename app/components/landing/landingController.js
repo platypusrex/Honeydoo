5f543ffd71a67250fe72cbbe51977c8a6fae9250
platypusrex <frankcooke79@gmail.com>
2016-03-25 23:32:03 -0400

    adding modal service dependency to landing page module to call registration modal

--- app/components/landing/landingController.js
+++ app/components/landing/landingController.js
@@ -1,7 +1,17 @@
 (function(landingModule){
     'use strict';
 
-    landingModule.controller('landingCtrl', ['$scope', function($scope){
-
+    landingModule.controller('landingCtrl', ['$scope','ModalService', function($scope, ModalService){
+        $scope.showRegister = function(){
+            ModalService.showModal({
+                templateUrl: "app/components/auth/register.html",
+                controller: "authCtrl"
+            }).then(function(modal) {
+                modal.element.modal();
+                modal.close.then(function(result) {
+                    console.log('hey dude')
+                });
+            });
+        };
     }]);
 }(angular.module('LandingModule')));
